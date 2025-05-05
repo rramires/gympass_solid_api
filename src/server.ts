1a8c74a05850da9dca265dcc3a494c70bb935607
rramires <ricardo@flexbr.com>
2025-05-05 16:37:54 -0300

    Install and config Fastfy.

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,13 @@
-const language: string = "TypeScript"
+import { app } from "./app";
 
-console.log(`Hello from ${language} !!!`);
+app.listen({
+    // '0.0.0.0' ensures access across all network interfaces
+    host: '0.0.0.0',
+    port: 3333
+}, (err, address) => {
+    if (err) {
+        console.error(err)
+        process.exit(1)
+    }
+    console.log(`Server is running at ${address}`)
+})
