b93dfddb7e2b55e6d8ea62a3f4f5eea481b3dd8a
rramires <ricardo@flexbr.com>
2025-05-09 13:59:28 -0300

    Install and config ESLint and Prettier.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
-import fastify from 'fastify';
+import fastify from 'fastify'
 
 export const app = fastify()
 
-app.get('/hello', () => { return 'Hello from Fastfy!' })
+app.get('/hello', () => {
+	return 'Hello from Fastfy!'
+})
 
 // test in your browser with http://localhost:3333/hello
