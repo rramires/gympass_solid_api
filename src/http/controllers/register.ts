e61bced8223de14e3ebed60e2e0e91da5a8e7c54
rramires <ricardo@flexbr.com>
2025-05-12 22:35:37 -0300

    Adding password hash.

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
 import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
 	const bodySchema = z.object({
@@ -10,11 +11,23 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 	})
 	const { name, email, password } = bodySchema.parse(request.body)
 
+	const userWithSameEmail = await prisma.user.findUnique({
+		where: {
+			email,
+		},
+	})
+	if (userWithSameEmail) {
+		// 409 Conflict
+		return reply.status(409).send()
+	}
+
+	const password_hash = await hash(password, 12)
+
 	await prisma.user.create({
 		data: {
 			name,
 			email,
-			password_hash: password,
+			password_hash,
 		},
 	})
 	return reply.status(201).send()
