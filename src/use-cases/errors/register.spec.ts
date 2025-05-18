386d3900d7f3902da1b9894924da7fb50efa0567
rramires <ricardo@flexbr.com>
2025-05-18 20:26:37 -0300

    Adding email unique test.

--- src/use-cases/errors/register.spec.ts
+++ src/use-cases/errors/register.spec.ts
@@ -2,6 +2,7 @@ import { expect, describe, it } from 'vitest'
 import { RegisterUseCase } from '../register-use-case'
 import { compare } from 'bcryptjs'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistsError } from './user-already-exists-error'
 
 describe('Register Use Case', () => {
 	it('should be possible to hash the password when registering a new user', async () => {
@@ -18,5 +19,22 @@ describe('Register Use Case', () => {
 
 		const isPasswordCorrectHashed = await compare(password, user.password_hash)
 		expect(isPasswordCorrectHashed).toBe(true)
+	})
+
+	it('should not be able to register with same email twice', async () => {
+		// in-memory mock database
+		const usersRepository = new InMemoryUsersRepository()
+		const registerUsecase = new RegisterUseCase(usersRepository)
+
+		const newUser = {
+			name: 'Jhon Doe',
+			email: 'jhondoe@email.com',
+			password: 'abc123',
+		}
+		// add
+		await registerUsecase.execute(newUser)
+
+		// add same email - return error
+		expect(registerUsecase.execute(newUser)).rejects.toBeInstanceOf(UserAlreadyExistsError)
 	})
 })
