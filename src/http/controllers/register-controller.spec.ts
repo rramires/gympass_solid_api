2f847cde5fa7dbdef5597821ac26d3464e24f461
rramires <ricardo@flexbr.com>
2025-06-06 15:02:02 -0300

    Adding register e2e test.

--- src/http/controllers/register-controller.spec.ts
+++ src/http/controllers/register-controller.spec.ts
@@ -1,5 +1,25 @@
-import { test, expect } from 'vitest'
+import { describe, beforeAll, afterAll, it, expect } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
 
-test('test e2e tests config', () => {
-	expect(1 + 1).toEqual(2)
+describe('Register (e2e)', () => {
+	beforeAll(async () => {
+		// running app
+		await app.ready()
+	})
+
+	afterAll(async () => {
+		// shutdown app
+		await app.close()
+	})
+
+	it('should be able to register', async () => {
+		const response = await request(app.server).post('/users').send({
+			name: 'John Doe',
+			email: 'johndoe@example.com',
+			password: '123456',
+		})
+
+		expect(response.statusCode).toEqual(201)
+	})
 })
